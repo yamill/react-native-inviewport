2b7bef54efcae0278a138200f81bfacaacfa4dac
Jay Harris <jay.harris@outlook.co.nz>
2021-04-09 14:38:29 +0200

    Change output directory

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import { ViewProps } from 'react-native';
-export interface Constraints {
-    aboveScreen?: boolean;
-    belowScreen?: boolean;
-}
 export interface InViewPortProps extends ViewProps {
     disabled?: boolean;
     interval?: number;
     onChange?: (visible: boolean) => void;
     children?: React.ReactNode;
-    constraints?: Constraints;
 }
 export interface Rect {
     top: number;
--- index.js
+++ index.js
@@ -25,24 +25,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 var react_1 = require("react");
 var react_native_1 = require("react-native");
-var defaultConstraints = {
-    aboveScreen: true,
-    belowScreen: true
-};
-var isInWindow = function (viewSize, constraints) {
+var isInWindow = function (viewSize) {
     var window = react_native_1.Dimensions.get('window');
     var isVisible = viewSize.bottom != 0 &&
-        (constraints.aboveScreen && viewSize.top) >= 0 &&
-        (constraints.belowScreen && viewSize.bottom <= window.height) &&
+        viewSize.top >= 0 &&
+        viewSize.bottom <= window.height &&
         viewSize.width > 0 &&
         viewSize.width <= window.width;
     return isVisible;
 };
 function InViewPort(props) {
-    var _a, _b;
     var disabled = props.disabled, interval = props.interval, viewProps = __rest(props, ["disabled", "interval"]);
-    var constraints = react_1.useMemo(function () { var _a; return (_a = props.constraints) !== null && _a !== void 0 ? _a : defaultConstraints; }, [(_a = props.constraints) === null || _a === void 0 ? void 0 : _a.aboveScreen, (_b = props.constraints) === null || _b === void 0 ? void 0 : _b.belowScreen]);
-    var _c = react_1.useState({ top: 0, bottom: 0, width: 0 }), rect = _c[0], setRect = _c[1];
+    var _a = react_1.useState({ top: 0, bottom: 0, width: 0 }), rect = _a[0], setRect = _a[1];
     var viewRef = react_1.useRef(null);
     var wasVisible = react_1.useRef(null);
     react_1.useEffect(function () {
@@ -59,7 +53,7 @@ function InViewPort(props) {
                     width: pageX + width
                 };
                 setRect(rect);
-                var visible = isInWindow(rect, constraints);
+                var visible = isInWindow(rect);
                 if (visible !== wasVisible.current) {
                     (_a = props.onChange) === null || _a === void 0 ? void 0 : _a.call(props, visible);
                     wasVisible.current = visible;
@@ -70,7 +64,7 @@ function InViewPort(props) {
             wasVisible.current = null;
             clearInterval(timer);
         };
-    }, [props.interval, props.disabled, constraints]);
+    }, [props.interval, props.disabled]);
     return React.createElement(react_native_1.View, __assign({ ref: viewRef, collapsable: false }, viewProps), props.children);
 }
 exports.default = InViewPort;
