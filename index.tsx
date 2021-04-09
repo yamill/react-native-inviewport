29e96eff146e4cace4c03017744334029b2236d0
Jay Harris <jay.harris@outlook.co.nz>
2021-04-09 14:37:31 +0200

    remove constraints - they were a bad idea

--- index.tsx
+++ index.tsx
@@ -2,22 +2,11 @@ import * as React from 'react'
 import { Component, useState, useEffect, useRef, useMemo } from 'react'
 import { View, Dimensions, ViewProps } from 'react-native'
 
-export interface Constraints {
-  aboveScreen?: boolean;
-  belowScreen?: boolean;
-}
-
-const defaultConstraints: Constraints = {
-  aboveScreen: true,
-  belowScreen: true
-};
-
 export interface InViewPortProps extends ViewProps {
   disabled?: boolean;
   interval?: number;
   onChange?: (visible: boolean) => void;
   children?: React.ReactNode;
-  constraints?: Constraints;
 }
 
 export interface Rect {
@@ -26,12 +15,12 @@ export interface Rect {
   width: number;
 }
 
-const isInWindow = (viewSize: Rect, constraints: Constraints) => {
+const isInWindow = (viewSize: Rect) => {
   const window = Dimensions.get('window');
   const isVisible =
     viewSize.bottom != 0 &&
-    (constraints.aboveScreen && viewSize.top) >= 0 &&
-    (constraints.belowScreen && viewSize.bottom <= window.height) &&
+    viewSize.top >= 0 &&
+    viewSize.bottom <= window.height &&
     viewSize.width > 0 &&
     viewSize.width <= window.width
   return isVisible;
@@ -39,7 +28,6 @@ const isInWindow = (viewSize: Rect, constraints: Constraints) => {
 
 export default function InViewPort(props: InViewPortProps) {
   const { disabled, interval, ...viewProps } = props;
-  const constraints = useMemo(() => props.constraints ?? defaultConstraints, [props.constraints?.aboveScreen, props.constraints?.belowScreen]);
 
   const [rect, setRect] = useState<Rect>({ top: 0, bottom: 0, width: 0 });
   const viewRef = useRef<View>(null);
@@ -59,7 +47,7 @@ export default function InViewPort(props: InViewPortProps) {
         };
         setRect(rect);
 
-        const visible = isInWindow(rect, constraints);
+        const visible = isInWindow(rect);
         if (visible !== wasVisible.current) {
           props.onChange?.(visible);
           wasVisible.current = visible;
@@ -71,7 +59,7 @@ export default function InViewPort(props: InViewPortProps) {
       wasVisible.current = null;
       clearInterval(timer);
     }
-  }, [props.interval, props.disabled, constraints]);
+  }, [props.interval, props.disabled]);
 
   return <View ref={viewRef} collapsable={false} {...viewProps}>
     {props.children}
